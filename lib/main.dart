da90faa044adee8f13a159ce857f7cb67403e032
NGHIA64 <83792258+NGHIA64@users.noreply.github.com>
2022-09-21 16:28:24 +0700

    array

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,10 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Màn hình hiển thị:',
+            ),
+            const Text(
+              'Màn hình hiển thị:',
             ),
             Text(
               '$_counter',
